ff2be9819459df0f332a19d045a879273149142f
useon <74897720+useon@users.noreply.github.com>
2021-12-30 00:00:12 +0900

    1만 시간의 법칙

---
+++ 1만시간의 법칙/script.js
@@ -0,0 +1,34 @@
+const btnStart = document.querySelector(".btn_start");
+const result = document.querySelector(".result")
+const resultJob = document.querySelector(".result_job");
+const resultTime = document.querySelector(".result_time");
+const inputJob = document.querySelector(".input_jab");
+const inputTime = document.querySelector(".input_time")
+const inputTime_int = Number(inputTime.value);
+
+result.style.display = "none";
+
+function calculator() {
+
+  if (inputJob.value == "") {
+    alert('입력해주세요!');
+    inputJob.focus();
+    return false;
+  } else if (inputTime.value == "") {
+    alert('입력해주세요!');
+    inputTime.focus();
+    return false;
+  } else if (inputTime_int > 24) {
+    alert('24이하의 값을 입력해 주세요!');
+    return false;
+  }
+
+  setTimeout(function () {
+    result.style.display = "block";
+    resultJob.innerText = inputJob.value;
+    resultTime.innerText = parseInt((10000 / inputTime_int), 10);
+  }, 1800);
+
+}
+
+btnStart.addEventListener("click", calculator);
